e42e3000a49d837409d850e188f68556e6cff86f
Cyryl Plotnicki-Chudyk <cyplo@cyplo.net>
2014-11-11 19:37:00 +0100

    reinitialize script now complete; can boot into accelerated gui box with no problems

--- reinitialize.sh
+++ reinitialize.sh
@@ -7,10 +7,23 @@ echo "BOOTING UP NEW ONES"
 vagrant up
 echo "MACHINES BOOTED UP AND SOFTWARE INSTALLED"
 echo "REFRESHING GRAPHICS AND NETWORK DRIVERS"
-sleep5 
-vagrant vbguest --force
-echo "REBOOTING"
+sleep 2 
+
 vagrant halt
 vagrant plugin install vagrant-vbguest
+
+#will update guest additions if the ones preinstalled are old
 vagrant up
+
+#if the guest additions are in the same version - reinstall
+#only for gui box, as there we care about the video drivers
+vagrant vbguest devbox --force rebuild
+
+# reboot to reload kernel with guest additions
+echo "REBOOTING"
+#
+vagrant halt
+vagrant up
+
+echo "YOUR DEV ENV IS READY"
 
