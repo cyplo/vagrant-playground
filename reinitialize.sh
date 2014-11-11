e41a54c1237b12f515ce65982cf6f0a4839416c8
Cyryl Plotnicki-Chudyk <cyplo@cyplo.net>
2014-11-11 18:59:45 +0100

    more verbose reinitialization script; using vagrant halt+up for final reboot

--- reinitialize.sh
+++ reinitialize.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
+set -e
+echo "DESTROYING OLD MACHINES"
 vagrant destroy -f
+echo "BOOTING UP NEW ONES"
 vagrant plugin install vagrant-vbguest
 vagrant up
+echo "MACHINES BOOTED UP AND SOFTWARE INSTALLED"
+echo "REFRESHING GRAPHICS AND NETWORK DRIVERS"
+sleep5 
 vagrant vbguest --force
-vagrant ssh -c "sudo reboot"
+echo "REBOOTING"
+vagrant halt
+vagrant up
 
