93a75dfc3a5ee0935aea97ed7afc5c10e8b2ebe9
Cyryl Plotnicki-Chudyk <cyplo@cyplo.net>
2014-11-11 18:34:56 +0100

    reboot after whole provisioning complete, use offline xamaring signing key

--- provision_mono.sh
+++ provision_mono.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-sudo rpm --import "https://pgp.mit.edu/pks/lookup?op=get&search=0x3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF"
+sudo rpm --import /vagrant/xamarin.asc
 sudo yum-config-manager --add-repo http://download.mono-project.com/repo/centos/
 sudo yum -y install mono
 
--- reinitialize.sh
+++ reinitialize.sh
@@ -3,4 +3,5 @@ vagrant destroy -f
 vagrant plugin install vagrant-vbguest
 vagrant up
 vagrant vbguest --force
+vagrant ssh -c "sudo reboot"
 
