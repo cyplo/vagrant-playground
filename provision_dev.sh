07acd405c2f51b5db2dda4023967fa884674d6d9
Cyryl Plotnicki-Chudyk <cyplo@cyplo.net>
2014-11-12 10:59:43 +0100

    removed unnecessary dev box reboot form the shell provisioner, as this is now done via vagrant halt & up

--- provision_dev.sh
+++ provision_dev.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 sudo yum install -y monodevelop 
 sudo yum -y groupinstall "GNOME desktop"
-sudo  ln -sf /lib/systemd/system/runlevel5.target /etc/systemd/system/default.target
-
-sudo reboot
+sudo ln -sf /lib/systemd/system/runlevel5.target /etc/systemd/system/default.target
 
